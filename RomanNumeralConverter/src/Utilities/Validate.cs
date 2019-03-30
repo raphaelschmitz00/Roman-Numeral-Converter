8c909d5acc1e8396cf952cd4d410cf093377b89b
Raphael Schmitz <raphaelschmitz00@gmail.com>
2019-03-30 20:25:20 +0100

    Implementing Validate utility class

--- RomanNumeralConverter/src/Utilities/Validate.cs
+++ RomanNumeralConverter/src/Utilities/Validate.cs
@@ -1,10 +1,14 @@
+using System;
+
+
 namespace RomanNumeralConverter.Utilities
 {
 	public static class Validate
 	{
-		public static T NotNull<T>(T value)
+		public static T NotNull<T>(T value) where T : class
 		{
-			throw new System.NotImplementedException();
+			if (ReferenceEquals(value, null)) throw new ArgumentNullException();
+			return value;
 		}
 	}
 }
