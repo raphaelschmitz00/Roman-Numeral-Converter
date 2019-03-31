55a2bb732a1233292d36e38f355d354ae4918ca2
Raphael Schmitz <raphaelschmitz00@gmail.com>
2019-03-31 13:05:42 +0200

    correcting some arabic to roman unit tests

--- RomanNumeralConverterUnitTests/src/Converters/ArabicToRomanConverterTests.cs
+++ RomanNumeralConverterUnitTests/src/Converters/ArabicToRomanConverterTests.cs
@@ -6,12 +6,20 @@ namespace RomanNumeralConverterUnitTests.Converters
 {
 	public class ArabicToRomanConverterTests
 	{
+		private ArabicToRomanConverter _arabicToRomanConverter;
+
+
+		[SetUp]
+		public void SetUp()
+		{
+			_arabicToRomanConverter = new ArabicToRomanConverter();
+		}
+
+
 		[Test]
 		public void CanConvertThree()
 		{
-			ArabicToRomanConverter arabicToRomanConverter = new ArabicToRomanConverter();
-
-			string threeInRoman = arabicToRomanConverter.Convert(3);
+			string threeInRoman = _arabicToRomanConverter.Convert(3);
 
 			Assert.AreEqual("III", threeInRoman);
 		}
@@ -20,9 +28,7 @@ namespace RomanNumeralConverterUnitTests.Converters
 		[Test]
 		public void CanConvertFour()
 		{
-			ArabicToRomanConverter arabicToRomanConverter = new ArabicToRomanConverter();
-
-			string inRoman = arabicToRomanConverter.Convert(4);
+			string inRoman = _arabicToRomanConverter.Convert(4);
 
 			Assert.AreEqual("IV", inRoman);
 		}
@@ -31,9 +37,7 @@ namespace RomanNumeralConverterUnitTests.Converters
 		[Test]
 		public void CanConvertSix()
 		{
-			ArabicToRomanConverter arabicToRomanConverter = new ArabicToRomanConverter();
-
-			string inRoman = arabicToRomanConverter.Convert(6);
+			string inRoman = _arabicToRomanConverter.Convert(6);
 
 			Assert.AreEqual("VI", inRoman);
 		}
@@ -42,9 +46,7 @@ namespace RomanNumeralConverterUnitTests.Converters
 		[Test]
 		public void CanConvertNine()
 		{
-			ArabicToRomanConverter arabicToRomanConverter = new ArabicToRomanConverter();
-
-			string inRoman = arabicToRomanConverter.Convert(9);
+			string inRoman = _arabicToRomanConverter.Convert(9);
 
 			Assert.AreEqual("IX", inRoman);
 		}
@@ -53,11 +55,108 @@ namespace RomanNumeralConverterUnitTests.Converters
 		[Test]
 		public void CanConvertTwelve()
 		{
-			ArabicToRomanConverter arabicToRomanConverter = new ArabicToRomanConverter();
-
-			string inRoman = arabicToRomanConverter.Convert(12);
+			string inRoman = _arabicToRomanConverter.Convert(12);
 
 			Assert.AreEqual("XII", inRoman);
+		}
+
+
+		[Test]
+		public void CanConvertThirtyNine()
+		{
+			string inRoman = _arabicToRomanConverter.Convert(39);
+
+			Assert.AreEqual("XXXIX.", inRoman);
+		}
+
+
+		[Test]
+		public void CanConvertOneHundredSixty()
+		{
+			string inRoman = _arabicToRomanConverter.Convert(160);
+
+			Assert.AreEqual("CLX", inRoman);
+		}
+
+
+		[Test]
+		public void CanConvertTwoHundredSeven()
+		{
+			string inRoman = _arabicToRomanConverter.Convert(207);
+
+			Assert.AreEqual(" CCVII", inRoman);
+		}
+
+
+		[Test]
+		public void CanConvertTwoHundredFortySix()
+		{
+			string inRoman = _arabicToRomanConverter.Convert(246);
+
+			Assert.AreEqual("CCXLVI", inRoman);
+		}
+
+
+		[Test]
+		public void CanConvertFourHundredTwentyOne()
+		{
+			string inRoman = _arabicToRomanConverter.Convert(421);
+
+			Assert.AreEqual("CDXXI", inRoman);
+		}
+
+
+		[Test]
+		public void CanConvertOneThousandSixtySix()
+		{
+			string inRoman = _arabicToRomanConverter.Convert(1066);
+
+			Assert.AreEqual("MLXVI", inRoman);
+		}
+
+
+		[Test]
+		public void CanConvertOneThousandSevenHundredSeventySix()
+		{
+			string inRoman = _arabicToRomanConverter.Convert(1776);
+
+			Assert.AreEqual("MDCCLXXVI", inRoman);
+		}
+
+
+		[Test]
+		public void CanConvertOneThousandNineHundredFiftyFour()
+		{
+			string inRoman = _arabicToRomanConverter.Convert(1954);
+
+			Assert.AreEqual("MCMLIV", inRoman);
+		}
+
+
+		[Test]
+		public void CanConvertOneThousandNineHundredNinety()
+		{
+			string inRoman = _arabicToRomanConverter.Convert(1990);
+
+			Assert.AreEqual("MCMXC", inRoman);
+		}
+
+
+		[Test]
+		public void CanConvertTwoThousandFourteen()
+		{
+			string inRoman = _arabicToRomanConverter.Convert(2014);
+
+			Assert.AreEqual("MMXIV", inRoman);
+		}
+
+
+		[Test]
+		public void CanConvertTwoThousandNineteen()
+		{
+			string inRoman = _arabicToRomanConverter.Convert(2019);
+
+			Assert.AreEqual("MMXIX", inRoman);
 		}
 	}
 }
